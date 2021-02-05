d8789076f6600eca45b2b5ed310d7c6b46a33b4a
stepin104974 <69135861+stepin-104974-sumit@users.noreply.github.com>
2021-02-05 12:52:47 +0530

    uploading main file

---
+++ Activity-2b/main.c
@@ -0,0 +1,26 @@
+#include"sumit.h"
+#include "christyjohnson.h"
+#include"dhyan.h"
+#include "harshita.h"
+#include "lakshita.h"
+#include"namratha.h"
+#include "Rishita.h"
+#include "satya.h"
+#include "srushti.h"
+#include <stdio.h>
+
+int main(){
+
+ christyjohnson();
+ sumit();
+ dhyan();
+ harshita();
+ lakshita();
+ namratha();
+ rishita();
+ satay();
+ srushti();
+ 
+
+
+}
